9ca8b504ea6c4058be4266079782f80dceecb68d
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-08-12 18:22:20 +0500

    guessing game completed

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-"use strict";
-console.log('this is me the first console of this programs');
+import inquirer from "inquirer";
+const GuessingGame = async () => {
+    for (let i = 0; i < 3; i++) {
+        const { num } = await inquirer.prompt([
+            {
+                type: 'number',
+                name: 'num',
+                message: 'Welcome to Guessing Game enter the number you have guessed'
+            }
+        ]);
+        const generated_num = Math.floor(Math.random() * 10); // Generated a random number between 1 to 10
+        if (generated_num === num) {
+            console.log(`You guessed right. the number is ${generated_num}`);
+            break;
+        }
+        else {
+            console.log('incorrect try again');
+        }
+    }
+};
+async function repeat() {
+    do {
+        await GuessingGame();
+        var { repeat } = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "repeat",
+                message: "Do you want to play again?",
+            },
+        ]);
+    } while (repeat);
+}
+// Calling the repeat  function
+repeat();
--- app.ts
+++ app.ts
@@ -1,6 +1,45 @@
 import inquirer from "inquirer";
 
+interface Inquirer{
+num:number,
+}
+
 const GuessingGame=async()=>{
 
-    const data=await inquirer.prompt([])
+for(let i=0;i<3;i++){
+
+    const {num}:Inquirer=await inquirer.prompt([
+        {
+            type:'number',
+            name:'num',
+            message:'Welcome to Guessing Game enter the number you have guessed'
+    
+        }
+    ])
+    
+    const generated_num= Math.floor(Math.random()*10) // Generated a random number between 1 to 10
+    
+    if(generated_num===num){
+        console.log(`You guessed right. the number is ${generated_num}`)
+        break;
+    }else{
+        console.log('incorrect try again')
+    }
 }
+
+}
+async function repeat() {
+    do {
+      await GuessingGame();
+      var { repeat }:string = await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "repeat",
+          message: "Do you want to play again?",
+        },
+      ]);
+    } while (repeat);
+  }
+  
+  // Calling the repeat  function
+  repeat();
